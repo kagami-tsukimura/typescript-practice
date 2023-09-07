e5fa6cd65ee1020a783964e9a591a31dee56062f
Kagami <shimura.kyohei7617@gmail.com>
2023-09-07 21:32:12 +0900

    style: npm run build

--- typescript/section3/dist/app.js
+++ typescript/section3/dist/app.js
@@ -1,11 +1,3 @@
-// object
-const profile = { name: 'Kagami', age: 28 };
-console.log(profile);
-const employee = { name: 'tsukimura', age: 26 };
-const employeeInfo = {
-    name: 'tsukimura',
-    age: 26,
-    hobby: 'swimming',
-};
-console.log(employeeInfo.hobby);
+"use strict";
+// DOM
 //# sourceMappingURL=app.js.map
