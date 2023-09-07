cb5a2b12c656d70db639cf1892fcf25e1aaf9740
Kagami <shimura.kyohei7617@gmail.com>
2023-09-07 21:40:31 +0900

    update: HTMLInputElement

--- typescript/section3/dist/app.js
+++ typescript/section3/dist/app.js
@@ -1,3 +1,10 @@
-// DOM
+// const input = document.getElementById('username');
+// HTML Element error
+// (Because it cannot be determined which tag is handled)
+// input.placeholder = 'Username';
+// Confirm the mold
 const input = document.getElementById('username');
+// Generics is also possible
+// const input =<HTMLInputElement> document.getElementById('username') as HTMLInputElement;
 input.placeholder = 'Username';
+//# sourceMappingURL=app.js.map
--- typescript/section3/src/app.ts
+++ typescript/section3/src/app.ts
@@ -1,5 +1,11 @@
-const input = document.getElementById('username');
+// const input = document.getElementById('username');
 
 // HTML Element error
-// (どのタグとして扱っているかを判別できないため)
+// (Because it cannot be determined which tag is handled)
 // input.placeholder = 'Username';
+
+// Confirm the mold
+const input = document.getElementById('username') as HTMLInputElement;
+// Generics is also possible
+// const input =<HTMLInputElement> document.getElementById('username') as HTMLInputElement;
+input.placeholder = 'Username';
